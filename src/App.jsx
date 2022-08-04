9dbb0c110be33aea1eb3be813e5a020970082778
unnati1004 <95961486+unnati1004@users.noreply.github.com>
2022-08-04 12:32:06 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+import Generator from "./Components/Generator";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Bio Generator</h1>
+       <Generator/>
+    </div>
+  );
+}
+
+export default App;
