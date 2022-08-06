522b93a35065573b79fb692bcea3f933fe6556bf
unnati1004 <95961486+unnati1004@users.noreply.github.com>
2022-08-06 19:21:18 +0530

    add random selection

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Generator from "./Components/Generator";
 function App() {
   return (
     <div className="App">
-      <div style={{backgroundColor:"rgb(146, 158, 129)"}}>
+      <div style={{backgroundColor:"rgb(185, 195, 170)"}}>
       <h1 style={{marginLeft:"40%"}}>Bio Generator</h1>
       </div>
       <div>
